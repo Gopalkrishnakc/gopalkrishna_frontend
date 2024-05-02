8f487206b2903200b96a4d9ee5acb20665003b40
Gopalkrishnakc <gopalkrishna6985@gmail.com>
2024-05-02 20:29:25 +0530

    message

---
+++ react_task/api_fetch/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+import FetchComp from './Components/Memo';
+function App() {
+  return (
+    <div >
+   
+ 
+      <FetchComp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_task/todo/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+// import ReducerComp from './Components/Reducer';
+import Todos from './Components/Crudreducer';
+
+function App() {
+  return (
+    <div >
+   
+ <Todos/>
+      
+    </div>
+  );
+}
+
+export default App;
